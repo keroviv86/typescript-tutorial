a904c2cb8b878697ac0c9a6401a61c3cc01e8e37
Vivian Yim <keroviv86@gmail.com>
2022-04-25 10:42:03 -0500

    first

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,33 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+let name: string;
+let age: number | string;
+let isStudent: boolean;
+let hobbies: number[];
+let role: [number, string]
+
+let printName: (name:string) => void;
+
+//void returns undefined
+//never doesnt return anything
+
+type Person = {
+  name:string;
+  age?: number  //question mark makes age optional
+}
+
+// let person: Person = {
+//   name: "Vivian"
+// }
+
+
+// let lotsOfPeople: Person[];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Hello World
     </div>
   );
 }
